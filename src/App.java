047cbfb5f49e82c40c215024a8e0da414966d8b9
Kuripipeer <mchristianaxel@gmail.com>
2024-02-27 23:05:18 -0600

    wenas

--- src/App.java
+++ src/App.java
@@ -18,19 +18,6 @@ public class App {
 
     private static String[][] CrearTablero() {
         String[][] tablero = new String[8][8];
-        boolean esBlanco = true;
-
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (esBlanco) {
-                    tablero[i][j] = "\u001B[47m" + " ";
-                } else {
-                    tablero[i][j] = "\u001B[40m" + " ";
-                }
-                esBlanco = !esBlanco;
-            }
-            esBlanco = !esBlanco;
-        }
 
         tablero[0][0] = "\u001B[47m" + "\u001B[35m" + "T";
         tablero[0][1] = "\u001B[40m" + "\u001B[35m" + "C";
@@ -40,7 +27,8 @@ public class App {
         tablero[0][5] = "\u001B[40m" + "\u001B[35m" + "A";
         tablero[0][6] = "\u001B[47m" + "\u001B[35m" + "C";
         tablero[0][7] = "\u001B[40m" + "\u001B[35m" + "T";
-        tablero[1][0] = "\u001B[47m" + "\u001B[35m" + "P";
+
+        tablero[1][0] = "\u001B[40m" + "\u001B[35m" + "P";
         tablero[1][1] = "\u001B[47m" + "\u001B[35m" + "P";
         tablero[1][2] = "\u001B[40m" + "\u001B[35m" + "P";
         tablero[1][3] = "\u001B[47m" + "\u001B[35m" + "P";
@@ -49,22 +37,59 @@ public class App {
         tablero[1][6] = "\u001B[40m" + "\u001B[35m" + "P";
         tablero[1][7] = "\u001B[47m" + "\u001B[35m" + "P";
 
-        tablero[6][0] = "\u001B[40m" + "\u001B[35m" + "p";
-        tablero[6][1] = "\u001B[47m" + "\u001B[35m" + "p";
-        tablero[6][2] = "\u001B[40m" + "\u001B[35m" + "p";
-        tablero[6][3] = "\u001B[47m" + "\u001B[35m" + "p";
-        tablero[6][4] = "\u001B[40m" + "\u001B[35m" + "p";
-        tablero[6][5] = "\u001B[47m" + "\u001B[35m" + "p";
-        tablero[6][6] = "\u001B[40m" + "\u001B[35m" + "p";
-        tablero[6][7] = "\u001B[47m" + "\u001B[35m" + "p";
+        tablero[2][0] = "\u001B[47m" + "\u001B[35m" + " ";
+        tablero[2][1] = "\u001B[40m" + "\u001B[35m" + " ";
+        tablero[2][2] = "\u001B[47m" + "\u001B[35m" + " ";
+        tablero[2][3] = "\u001B[40m" + "\u001B[35m" + " ";
+        tablero[2][4] = "\u001B[47m" + "\u001B[35m" + " ";
+        tablero[2][5] = "\u001B[40m" + "\u001B[35m" + " ";
+        tablero[2][6] = "\u001B[47m" + "\u001B[35m" + " ";
+        tablero[2][7] = "\u001B[40m" + "\u001B[35m" + " ";
+
+        tablero[3][0] = "\u001B[40m" + "\u001B[35m" + " ";
+        tablero[3][1] = "\u001B[47m" + "\u001B[35m" + " ";
+        tablero[3][2] = "\u001B[40m" + "\u001B[35m" + " ";
+        tablero[3][3] = "\u001B[47m" + "\u001B[35m" + " ";
+        tablero[3][4] = "\u001B[40m" + "\u001B[35m" + " ";
+        tablero[3][5] = "\u001B[47m" + "\u001B[35m" + " ";
+        tablero[3][6] = "\u001B[40m" + "\u001B[35m" + " ";
+        tablero[3][7] = "\u001B[47m" + "\u001B[35m" + " ";
+
+        tablero[4][0] = "\u001B[47m" + "\u001B[35m" + " ";
+        tablero[4][1] = "\u001B[40m" + "\u001B[35m" + " ";
+        tablero[4][2] = "\u001B[47m" + "\u001B[35m" + " ";
+        tablero[4][3] = "\u001B[40m" + "\u001B[35m" + " ";
+        tablero[4][4] = "\u001B[47m" + "\u001B[35m" + " ";
+        tablero[4][5] = "\u001B[40m" + "\u001B[35m" + " ";
+        tablero[4][6] = "\u001B[47m" + "\u001B[35m" + " ";
+        tablero[4][7] = "\u001B[40m" + "\u001B[35m" + " ";
+
+        tablero[5][0] = "\u001B[40m" + "\u001B[35m" + " ";
+        tablero[5][1] = "\u001B[47m" + "\u001B[35m" + " ";
+        tablero[5][2] = "\u001B[40m" + "\u001B[35m" + " ";
+        tablero[5][3] = "\u001B[47m" + "\u001B[35m" + " ";
+        tablero[5][4] = "\u001B[40m" + "\u001B[35m" + " ";
+        tablero[5][5] = "\u001B[47m" + "\u001B[35m" + " ";
+        tablero[5][6] = "\u001B[40m" + "\u001B[35m" + " ";
+        tablero[5][7] = "\u001B[47m" + "\u001B[35m" + " ";
+
+        tablero[6][0] = "\u001B[47m" + "\u001B[35m" + "p";
+        tablero[6][1] = "\u001B[40m" + "\u001B[35m" + "p";
+        tablero[6][2] = "\u001B[47m" + "\u001B[35m" + "p";
+        tablero[6][3] = "\u001B[40m" + "\u001B[35m" + "p";
+        tablero[6][4] = "\u001B[47m" + "\u001B[35m" + "p";
+        tablero[6][5] = "\u001B[40m" + "\u001B[35m" + "p";
+        tablero[6][6] = "\u001B[47m" + "\u001B[35m" + "p";
+        tablero[6][7] = "\u001B[40m" + "\u001B[35m" + "p";
+
         tablero[7][0] = "\u001B[40m" + "\u001B[35m" + "t";
         tablero[7][1] = "\u001B[47m" + "\u001B[35m" + "c";
         tablero[7][2] = "\u001B[40m" + "\u001B[35m" + "a";
-        tablero[7][3] = "\u001B[47m" + "\u001B[35m" + "d";
-        tablero[7][4] = "\u001B[40m" + "\u001B[35m" + "r";
+        tablero[7][3] = "\u001B[47m" + "\u001B[35m" + "r";
+        tablero[7][4] = "\u001B[40m" + "\u001B[35m" + "d";
         tablero[7][5] = "\u001B[47m" + "\u001B[35m" + "a";
         tablero[7][6] = "\u001B[40m" + "\u001B[35m" + "c";
-        tablero[7][7] = "\u001B[47m" + "\u001B[35m" + "t";
+        tablero[7][7] = "\u001B[47m" + "\u001B[35m" + "t " + "\u001B[0m";
 
         return tablero;
     }
